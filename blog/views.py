81e7744e1503caa0f835dac98a32baaa3afa44ad
dsb00715 <dsb00715@gmail.com>
2019-04-19 13:44:19 +0200

    Created testdatabase and used Posts directly from DB instead of static values. Also added Post to admin page

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'Deep Bhatt',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'April 18, 2019'
-    },
-    {
-        'author' : 'Shraddha Bhatt',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'April 19, 2019'
-    },
-    {
-        'author' : 'Adwait Pandya',
-        'title' : 'Blog Post 3',
-        'content' : 'third post content',
-        'date_posted' : 'April 20, 2019'
-    },
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
