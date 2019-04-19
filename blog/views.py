d748b23b3038d1f7568c521e080c60ea7258ec2a
dsb00715 <dsb00715@gmail.com>
2019-04-19 16:15:17 +0200

    Complete authorization pages and updated login/logout pages and routes for them perfectly.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+@login_required
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
