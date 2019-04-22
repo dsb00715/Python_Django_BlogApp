1047ae39a504d00d24eca6f73f498caa6fb862b0
dsb00715 <dsb00715@gmail.com>
2019-04-22 07:39:10 +0200

    Options like Post Create, Update, delete are created and so the buttons for them.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
 
 @login_required
 def home(request):
@@ -8,6 +17,58 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(LoginRequiredMixin, ListView):
+    """A view that lists out all the posts"""
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    """A view that lists out detailed post."""
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """A view that lists out all the posts"""
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """A view that lists out all the posts"""
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """A view that lists out detailed post."""
+    model = Post
+    success_url = 'blog/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 @login_required
 def about(request):
