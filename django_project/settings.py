65d13766e0d76c52fa9a41e8adcff421a960071b
dsb00715 <dsb00715@gmail.com>
2019-04-27 12:30:29 +0200

    Password forget - send email address feature added . Related changes in the appropriate view

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,3 +131,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dsb007151@gmail.com'
+EMAIL_HOST_PASSWORD = 'mahadev24041'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
